b241b9ab1c22f829e934f8229a51f2367ebf5deb
sonu_kumar <sonu.kumar@mobiloitte.in>
2018-01-22 13:06:38 +0530

    service configuration on client

--- client/components/signup/signupCtrl.js
+++ client/components/signup/signupCtrl.js
@@ -1,7 +1,92 @@
-app.controller('signupCtrl', ['$scope', function($scope,userServices){
+app.controller('signupCtrl', function($scope,userService){
 	$scope.myform ={};
-	userServices.signup($scope.myform).then(success=>{
-		console.log('success-->>',success);
-	}).catch(err=>console.log('err-->>',err))
+	let log= console.log;
+	log('$scope.myform--->>',$scope.myform);
+	
+	 /** get selected image path *******/
+	$scope.loadFile = function(event) {
+     var output = document.getElementById('output');
+     //console.log(event.target.files[0])
+     output.src = URL.createObjectURL(event.target.files[0]); 
+     log("output src---->>",output); 
+    };
 
-}])
+	/**** convet base64 of selected image ******/   
+    $scope.changeimg = function(element){ 
+       var file=element.files[0]
+    if(file!=undefined){
+       var name = file.name;
+                    var ext = name.split(".").pop();
+                    var ext = ext.toUpperCase();
+                    var image_ext =['JPG' ,'JPEG' ,'PNG' ,'GIF' ,'TIF'];
+                    var index = image_ext.indexOf(ext);
+            if(index==-1){
+                        $scope.invalid_image = true;
+                        $.notify('Invalid image type selected','danger');
+            }else{
+                    $scope.invalid_image = false;
+                    var r = new FileReader();
+                    r.readAsDataURL(file);
+                    r.onloadend = function(e) {
+                                  $scope.$apply();
+                                   var base64img = e.target.result;
+                                  $scope.myform.image = base64img.substring(22);
+                              }
+        }
+    }else{
+            $scope.myform.image = '';
+            } 
+    }; 
+    /*$scope.imageChange=function(element){
+		var file=element.files[0]
+		if(file!=undefined){
+			 var name = file.name;
+                    var ext = name.split(".").pop();
+                    var ext = ext.toUpperCase();
+                    var image_ext =['JPG' ,'JPEG' ,'PNG' ,'GIF' ,'TIF'];
+                    var index = image_ext.indexOf(ext);
+            if(index==-1){
+                        $scope.invalid_image = true;
+                        $.notify('Invalid image type selected','danger');
+            }else{
+                    $scope.invalid_image = false;
+                    var r = new FileReader();
+                    r.readAsDataURL(file);
+                    r.onloadend = function(e) {
+                   			 					$scope.$apply();
+                   			 					 var base64img = e.target.result;
+                    							$scope.myform.image = base64img.substring(22);
+                							}
+				}
+		}else{
+            $scope.myform.image = '';
+            }
+	}*/
+   $scope.signup = function(){
+ 	console.log($scope.myForm);
+ 	//$scope.myForm.type = 'Admin';
+  // blockUI.start();
+ 	/*adminService.employee_signUp($scope.myForm).then(function(response){
+ 		console.log(response);
+     blockUI.stop();
+ 		if(response.data.response_code == 200){
+           if(response.data.result.type == 'Admin'){
+               $state.go('header.accessLevelManagement', {'userId':response.data.result._id });  
+           }else{
+               $state.go('header.accessLevelManagement', {'userId':response.data.result._id });  
+           }
+           $.notify(response.data.response_message);
+         
+ 		}else if(response.data.response_code == 500){
+       $.notify('Connection error' , 'danger');
+    }else{
+      $.notify(response.data.response_message , 'danger');
+
+ 		}
+ 	})*/
+ 	userService.signup($scope.myform).then(success=>{
+		console.log('success-->>',success);
+	}).catch(err=>console.log('err-->>',err));
+ } 
+   
+});
--- client/js/app.js
+++ client/js/app.js
@@ -1,1 +1,1 @@
-let app = angular.module('myApp',['ui-router']);
+let app = angular.module('myApp',['ui.router']);
